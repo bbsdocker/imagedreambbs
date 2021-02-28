53c934fc25fcc72c0f312f57cc3ff715ad235f78
Sean Ho <holishing@ccns.ncku.edu.tw>
2021-02-28 19:26:48 +0800

    correct order for loading env file

--- build_dreambbs.bash
+++ build_dreambbs.bash
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-cp /tmp/env.compile ~/.env.compile
-. ~/.env.compile
 if [ "${DREAMBBS_SNAP_GIT}" = "" ]; then export DREAMBBS_SNAP_GIT="https://github.com/ccns/dreambbs_snap.git"; fi
 if [ "${DREAMBBS_GIT}" = "" ]; then export DREAMBBS_GIT="https://github.com/ccns/dreambbs.git"; fi
 
 set -eux
 
+## clone current repo, build and install it
+git clone ${DREAMBBS_SNAP_GIT} ${BBSHOME}
+rm -rf ${BBSHOME}/.git*
+cp /tmp/env.compile ~/.env.compile
+. ~/.env.compile
+
 # check environment
 env
 gcc -v
 
-## clone current repo, build and install it
-git clone ${DREAMBBS_SNAP_GIT} ${BBSHOME}
-rm -rf ${BBSHOME}/.git*
 git clone ${DREAMBBS_GIT} ${BBSHOME}/src
 cp -v /tmp/dreambbs.conf ${BBSHOME}/src/dreambbs.conf
 echo 'export BBSHOME=${HOME}' > ${HOME}/.bashrc
